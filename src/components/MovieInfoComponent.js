01314b3e10479cfd3d6f7a2ca9dc76b33be7c15b
includeshubhamgenius <109446081+includeshubhamgenius@users.noreply.github.com>
2024-09-12 16:46:50 +0530

    Update project files for Film Finder app

---
+++ src/components/MovieInfoComponent.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { API_KEY } from "../App";
+import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 20px 30px;
+  align-items:center;
+  justify-content: center;
+  border-bottom: 1px solid lightgray;
+`;
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 350px;
+`;
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+`;
+const MovieName = styled.span`
+  font-size: 22px;
+  font-weight: 600;
+  color: black;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+  & span {
+    opacity: 0.8;
+  }
+`;
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  overflow: hidden;
+  margin: 4px 0;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+
+  & span {
+    opacity: 0.5;
+  }
+`;
+const CloseIcon = styled(FontAwesomeIcon)`
+  
+  height:20px;
+  width:20px;
+  color: red;
+  position: absolute;
+  top:160px;
+  right:25px;
+  border-radius:50%;
+  background: black;
+  padding: 8px;
+  cursor: pointer;
+`;
+const MovieInfoComponent = (props) => {
+  const [movieInfo, setMovieInfo] = useState();
+  const { selectedMovie } = props;
+
+  useEffect(() => {
+    Axios.get(
+      `https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`,
+    ).then((response) => setMovieInfo(response.data));
+  }, [selectedMovie]);
+  return (
+    <Container>
+      {movieInfo ? (
+        <>
+          <CoverImage src={movieInfo?.Poster} alt={movieInfo?.Title} />
+          <InfoColumn>
+            <MovieName>
+              {movieInfo?.Type}: <span>{movieInfo?.Title}</span>
+            </MovieName>
+            <MovieInfo>
+              IMDB Rating: <span>{movieInfo?.imdbRating}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Year: <span>{movieInfo?.Year}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Language: <span>{movieInfo?.Language}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Rated: <span>{movieInfo?.Rated}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Released: <span>{movieInfo?.Released}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Runtime: <span>{movieInfo?.Runtime}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Genre: <span>{movieInfo?.Genre}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Director: <span>{movieInfo?.Director}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Actors: <span>{movieInfo?.Actors}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Plot: <span>{movieInfo?.Plot}</span>
+            </MovieInfo>
+          </InfoColumn>
+          <CloseIcon onClick={() => props.onMovieSelect()} icon={faXmark} />
+
+        </>
+      ) : (
+        "Loading..."
+      )}
+    </Container>
+  );
+};
+export default MovieInfoComponent;
